1566b7758afa3afcefb75b4bce5aa95ffeecf5c9
vinaysharma <vs864606@gmail.com>
2025-04-01 02:49:11 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,19 @@ function App() {
       if(ele.target.classList.contains("fa-moon")){
         ele.target.classList.add("fa-sun");
         ele.target.classList.remove("fa-moon");
+        ele.target.classList.add("text-yellow-400")
+        ele.target.classList.remove("text-gray-800")
       }
       else{
         ele.target.classList.add("fa-moon");
         ele.target.classList.remove("fa-sun");
+        ele.target.classList.add("text-gray-800")
+        ele.target.classList.remove("text-yellow-400")
       }
-      ele.target.classList.toggle("text-yellow-400")
   };
   return (
     <>
-      <i  onClick={(e)=>{toggleDarkMode(e)}} className="fa-regular fa-moon cursor-pointer fixed top-2 right-2 text-2xl text-gray-800 z-1"></i>
+      <i  onClick={(e)=>{toggleDarkMode(e)}} className="fa-regular fa-sun cursor-pointer fixed top-2 right-2 text-2xl text-yellow-400 z-1"></i>
       <Home/>
       <About />
       <Education />
